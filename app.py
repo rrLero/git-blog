3877d322ae8e6eaf93f596dbc023c30440891136
rrlero <roman@xlan.net.ua>
2017-02-21 22:21:50 +0200

    git-blog Flask skeleton

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/index')
+@app.route('/')
+def homepage():
+    return render_template('base.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
