40eacc4efff1232a368500f59075db48086ebc76
rrlero <roman@xlan.net.ua>
2017-02-26 21:14:44 +0200

    Новые возможности, работа с файлами, работа с тегами и т.д. Описал все в блоге

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, session, flash, redirect, url_for
 import datetime
 import urllib.request
@@ -12,7 +13,8 @@ app.config.update(dict(
     SECRET_KEY='development key',
 ))
 
-
+f = open('static/temp.txt', 'w')
+f.close()
 # url = 'https://raw.githubusercontent.com/rrlero/git-blog-content/master'
 # x = urllib.request.urlopen('https://api.github.com/repos/rrlero/git-blog-content/contents/')
 # y = x.read()
@@ -24,7 +26,7 @@ app.config.update(dict(
 # считываем с репозитория git-blog-content файл README.md и читаем в переменную file
 def get_file(git_name, git_repository):
     list_git_files = []
-    git_objects = requests.get('https://api.github.com/repos/%s/%s/contents/' % (git_name, git_repository))
+    git_objects = requests.get('https://api.github.com/repos/%s/%s/contents/' % (git_name, git_repository), auth=('rrlero', '7M7T9nHH'))
     git_objects = git_objects.json()
     if str(type(git_objects)) == "<class 'dict'>":
         session['logged_in'] = False
@@ -39,6 +41,7 @@ def get_file(git_name, git_repository):
             val['date'] = git_object['name']
         try:
             val['date'] = datetime.datetime.strptime(val['date'], "%H:%M %d-%m-%Y")
+            val['date'] = str(val['date'])
         except:
             val['date'] = 'No date'
         val['title'] = 'No title'
@@ -47,6 +50,7 @@ def get_file(git_name, git_repository):
         data = [i for i in data.split('\n')]
         data.remove('')
         val['text'] = ''
+        val['tegs'] = 'No tegs'
         if len(data) > 5:
             for i in range(5):
                 key, string = test_string(data[i])
@@ -56,7 +60,10 @@ def get_file(git_name, git_repository):
         else:
             val['text'] = 'your file not in right format'
         list_git_files.append(val)
-    return sorted(list_git_files, key=lambda d: str(d['date']), reverse=True)
+        f = open('static/temp.txt', 'w')
+        f.write(json.dumps(list_git_files))
+        f.close()
+    return sorted(list_git_files, key=lambda d: d['date'], reverse=True)
 
 
 def test_string(test):
@@ -65,19 +72,23 @@ def test_string(test):
     if 'title' in test and ':' in test:
         return 'title', test.split(':')[1].strip()
     if 'tegs' in test and ':' in test:
-        return 'tegs', test.split(':')[1]
+        return 'tegs', test.split(':')[1].strip()
     if 'layout' in test and ':' in test:
-        return 'layout', test.split(':')[1]
+        return 'layout', test.split(':')[1].strip()
 
 
 @app.route('/index')
 @app.route('/')
 def homepage():
+    f = open('static/temp.txt', 'w')
+    f.close()
     return render_template('base.html')
 
 
 @app.route('/logout')
 def logout():
+    f = open('static/temp.txt', 'w')
+    f.close()
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('homepage'))
@@ -95,12 +106,26 @@ def login():
         return redirect(url_for('homepage'))
 
 
-@app.route('/<git_name>/<git_repository_blog>/')
-def blog(git_name, git_repository_blog):
+@app.route('/<git_name>/<git_repository_blog>/', methods=['GET', 'POST'])
+def blog(git_name, git_repository_blog, sort=None):
+    if request.method == 'GET':
+        sort = request.args.get('teg')
+        if sort == 'None':
+            sort = None
     session['logged_in'] = True
-    file = get_file(git_name, git_repository_blog)
+    f = open('static/temp.txt')
+    temp = f.readline()
+    if temp:
+        file = sorted(json.loads(temp), key=lambda d: d['date'], reverse=True)
+        tegs = {i['tegs'] for i in file}
+        f.close()
+        return render_template('blog.html', git_name=git_name, git_repository_blog=git_repository_blog, file=file,
+                               tegs=tegs, sort=sort)
+    else:
+        file = get_file(git_name, git_repository_blog)
+        tegs = {i['tegs'] for i in file}
     if file:
-        return render_template('blog.html', git_name=git_name, git_repository_blog=git_repository_blog, file=file)
+        return render_template('blog.html', git_name=git_name, git_repository_blog=git_repository_blog, file=file, tegs=tegs, sort=sort)
     else:
         session['logged_in'] = False
         flash('No such name or repository or both')
