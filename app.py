bbf5b2a755e298c7a23a3b2c2bdf9c431973543f
rrlero <roman@xlan.net.ua>
2017-03-01 23:04:29 +0200

    crossed zaprosy

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ def get_file(git_name, git_repository):
     f = open('static/%s.txt' % git_name, 'w')
     f.close()
     list_git_files = []
-    git_objects = requests.get('https://api.github.com/repos/%s/%s/contents/posts/' % (git_name, git_repository), auth=('rrlero', '7M7T9nHH'))
+    git_objects = requests.get('https://api.github.com/repos/%s/%s/contents/posts/' % (git_name, git_repository))
     git_objects = git_objects.json()
     if str(type(git_objects)) == "<class 'dict'>":
         session['logged_in'] = False
